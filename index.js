dff125ce17a7fb4576e71af744dfab306ea825bd
chanyoungcoding <white1614@naver.com>
2023-06-24 23:35:21 +0900

    routing add

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req,res) => {
   res.send('hello world!!')
 })
 
-app.listen(3000, () => {
-  console.log("Server Start!!")
+app.get('*', (req,res) => {
+  res.send('잘못된 페이지이거나, 오류가 발생했습니다.')
+})
+
+app.listen(8080, () => {
+  console.log("Server 8080 Start!!")
 })
