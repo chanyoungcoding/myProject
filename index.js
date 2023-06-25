ed9aae4f51221331436e775f9c26a1c85584391e
chanyoungcoding <white1614@naver.com>
2023-06-25 12:55:47 +0900

    ejs add

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express');
 
 const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "/views")); 
 
 app.get('/', (req,res) => {
-  res.send('hello world!!')
+  res.render('index')
+})
+
+
+//params
+app.get('/r/:subreddit/:postId', (req,res) => {
+  const {subreddit, postId} = req.params;
+  console.log(req.params) 
+  res.send(`this is ${subreddit} and ${postId}`);
+})
+
+//query
+app.get('/cat', (req,res) => {
+  console.log(req.query);
+  res.send('query')
 })
 
 app.get('*', (req,res) => {
