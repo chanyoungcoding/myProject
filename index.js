392abbffcfcde9a06b3bb4642314a5292306787f
chanyoungcoding <white1614@naver.com>
2023-07-02 12:26:24 +0900

    캠핑장추가기능생성

--- index.js
+++ index.js
@@ -35,6 +35,16 @@ app.get('/campgrounds', async(req,res) => {
   res.render('campgrounds/home', {campgrounds})
 })
 
+app.get('/campgrounds/new', (req,res) => {
+  res.render('campgrounds/new')
+})
+
+app.post('/campgrounds', async(req,res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect('/campgrounds');
+})
+
 app.get('/campgrounds/:id', async(req,res) => {
   const campground = await Campground.findById(req.params.id);
   res.render('campgrounds/show', {campground})
