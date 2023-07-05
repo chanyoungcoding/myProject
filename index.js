b1ba27cb7b551943e34f1842c4640ec5641dfef2
chanyoungcoding <white1614@naver.com>
2023-07-05 19:39:07 +0900

    비동기오류해결설정

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const Campground = require('./models/campground');
-const AppError = require('./app');
+const catchAsync = require('./utils/catchAsync');
 
 
 //MongoDB 연결
@@ -30,61 +30,52 @@ app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set("views", path.join(__dirname, "/views"));
 
-//middleWare
-
-const verifyPassword = (req,res,next) => {
-  const { password } = req.query;
-  if(password === '123') {
-    next();
-  }
-  throw new Error('패스워드가 필요합니다.')
-}
-
 //Route
 
-app.get('/campgrounds', async (req,res) => {
-  const campgrounds = await Campground.find({});
-  res.render('home', {campgrounds})
-})
+app.get("/campgrounds", catchAsync(async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render("home", { campgrounds });
+  }));
 
 app.get('/campgrounds/new', (req,res) => {
   res.render('campgrounds/new')
 })
 
-app.post('/campgrounds', async (req,res,next) => {
-  try{
+app.post("/campgrounds", catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect("/campgrounds");
-  } catch(e) {
-    return next(e)
-  }
-})
+  }));
 
-app.get('/campgrounds/:id', async (req,res,next) => {
-  const campground = await Campground.findById(req.params.id);
-  if(!campground) {
-    return next(new AppError('campground not Found', 404))
-  }
-  res.render('campgrounds/show', {campground})
-})
+app.get("/campgrounds/:id", catchAsync(async (req, res, next) => {
+    const campground = await Campground.findById(req.params.id);
+    if (!campground) {
+      return next(new AppError("campground not Found", 404));
+    }
+    res.render("campgrounds/show", { campground });
+  }));
 
-app.get("/campgrounds/:id/edit", async (req, res) => {
-  const campground = await Campground.findById(req.params.id);
-  res.render("campgrounds/edit", { campground });
-});
+app.get("/campgrounds/:id/edit", catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render("campgrounds/edit", { campground });
+  })
+);
 
-app.put("/campgrounds/:id", async (req,res) => {
-  const { id } =req.params;
-  const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
-  res.redirect(`/campgrounds/${campground._id}`)
-});
+app.put("/campgrounds/:id", catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {
+      ...req.body.campground,
+    });
+    res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
 
-app.delete('/campgrounds/:id', async (req,res) => {
-  const { id } = req.params;
-  await Campground.findByIdAndDelete(id);
-  res.redirect('/campgrounds')
-})
+app.delete("/campgrounds/:id", catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect("/campgrounds");
+  })
+);
 
 app.get('*', (req,res) => {
   res.send('잘못된 페이지이거나, 오류가 발생했습니다.')
