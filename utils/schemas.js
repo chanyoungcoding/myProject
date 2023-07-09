b0d5b0eb64102b193e92a0b20e1936406eb13612
chanyoungcoding <white1614@naver.com>
2023-07-09 20:18:16 +0900

    유효성검사보완 및 show 페이지 수정

--- utils/schemas.js
+++ utils/schemas.js
@@ -12,9 +12,9 @@ const campgroundSchema = Joi.object({
 
 const reviewSchema = Joi.object({
   review: Joi.object({
-    rating: Joi.number().required(),
+    rating: Joi.number().required().min(1).max(5),
     body: Joi.string().required()
-  })
+  }).required()
 })
 
 module.exports = {
