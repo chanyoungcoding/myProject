316f2fa272c0be24be77e140c0ab49ab9b0165bc
chanyoungcoding <white1614@naver.com>
2023-07-13 12:54:50 +0900

    라우터 분리

---
+++ Router/campgrounds.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const router = express.Router();
+
+//내가 불러온 것
+const Campground = require("../models/campground");
+const catchAsync = require("../utils/catchAsync");
+const ExpressError = require("../utils/ExpressError");
+const { campgroundSchema } = require("../utils/schemas");
+
+
+//Joi 유효성 검사
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((x) => x.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
+//Route
+
+router.get(
+  "/",
+  catchAsync(async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render("home", { campgrounds });
+  })
+);
+
+router.get("/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
+router.post(
+  "/",
+  validateCampground,
+  catchAsync(async (req, res, next) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    req.flash("success", "made it");
+    res.redirect("/campgrounds");
+  })
+);
+
+router.get(
+  "/:id",
+  catchAsync(async (req, res, next) => {
+    const campground = await Campground.findById(req.params.id).populate(
+      "reviews"
+    );
+    res.render("campgrounds/show", { campground });
+  })
+);
+
+router.get(
+  "/:id/edit",
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render("campgrounds/edit", { campground });
+  })
+);
+
+router.put(
+  "/:id",
+  validateCampground,
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {
+      ...req.body.campground,
+    });
+    res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
+
+router.delete(
+  "/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect("/campgrounds");
+  })
+);
+
+module.exports = router
--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const Campground = require('./models/campground');
 const Review = require('./models/review');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
-const {campgroundSchema, reviewSchema} = require('./utils/schemas');
+const reviewSchema = require('./utils/schemas');
+const campgrounds = require('./Router/campgrounds')
 
 //MongoDB 연결
 const mongoose = require('mongoose');
@@ -47,17 +48,6 @@ app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set("views", path.join(__dirname, "/views"));
 
-//Joi 유효성 검사
-
-const validateCampground = (req,res,next) => {
-  const { error } = campgroundSchema.validate(req.body);
-  if(error) {
-    const msg = error.details.map(x => x.message).join(',');
-    throw new ExpressError(msg, 400)
-  } else {
-    next();
-  }
-}
 
 const validateReview = (req, res, next) => {
   const { error } = reviewSchema.validate(req.body);
@@ -69,47 +59,7 @@ const validateReview = (req, res, next) => {
   }
 };
 
-//Route
-
-app.get("/campgrounds", catchAsync(async (req, res) => {
-  const campgrounds = await Campground.find({});
-  res.render("home", { campgrounds });
-}));
-
-app.get('/campgrounds/new', (req,res) => {
-  res.render('campgrounds/new')
-})
-
-app.post("/campgrounds", validateCampground ,catchAsync(async (req, res, next) => {
-  const campground = new Campground(req.body.campground);
-  await campground.save();
-  req.flash('success', 'made it');
-  res.redirect("/campgrounds");
-}));
-
-app.get("/campgrounds/:id", catchAsync(async (req, res, next) => {
-  const campground = await Campground.findById(req.params.id).populate('reviews');
-  res.render("campgrounds/show", { campground });
-}));
-
-app.get("/campgrounds/:id/edit", catchAsync(async (req, res) => {
-  const campground = await Campground.findById(req.params.id);
-  res.render("campgrounds/edit", { campground });
-}));
-
-app.put("/campgrounds/:id", validateCampground , catchAsync(async (req, res) => {
-  const { id } = req.params;
-  const campground = await Campground.findByIdAndUpdate(id, {
-    ...req.body.campground,
-  });
-  res.redirect(`/campgrounds/${campground._id}`);
-}));
-
-app.delete("/campgrounds/:id", catchAsync(async (req, res) => {
-  const { id } = req.params;
-  await Campground.findByIdAndDelete(id);
-  res.redirect("/campgrounds");
-}));
+app.use('/campgrounds', campgrounds);
 
 app.post('/campgrounds/:id/reviews', validateReview , catchAsync(async(req,res) => {
   const { id } = req.params;
