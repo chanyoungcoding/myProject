022614153a65abf306d8e6e00c89183ea79b31e4
chanyoungcoding <white1614@naver.com>
2023-07-24 11:01:25 +0900

    user controllers create

--- Router/user.js
+++ Router/user.js
@@ -2,45 +2,23 @@ const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const passport = require('passport');
-const User = require('../models/user');
+const user = require('../controllers/users')
 
-router.get('/register', (req,res) => {
-    res.render('users/register');
-})
+router.get('/register', user.registerUser);
 
-router.get('/login', (req,res) => {
-    res.render('users/login');
-})
+router.get('/login', user.loginUser)
 
-router.get('/logout', (req, res, next) => {
-    req.logout(function (err) {
-        if (err) {
-            return next(err);
-        }
-        req.flash('success', 'Goodbye!');
-        res.redirect('/campgrounds');
-    });
-});
+router.get('/logout', user.logoutUser);
 
-router.post('/register', catchAsync(async(req,res) => {
-    try{
-        const {username, email, password} = req.body;
-        const user = new User({email, username});
-        const registeredUser = await User.register(user, password);
-        req.login(registeredUser, err => {
-            if(err) return next(err)
-            req.flash('success', 'register success')
-            res.redirect('/campgrounds')  
-        })
-    } catch(e) {
-        req.flash('error', e.message)
-        res.redirect('register')
-    }
-}))
+router.post('/register', catchAsync(user.userRegister))
 
-router.post('/login',passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), (req,res) => {
-    req.flash("success", "made it");
-    res.redirect('/campgrounds')
-})
+router.post(
+  '/login',
+  passport.authenticate(
+    'local',
+    {failureFlash: true, failureRedirect: '/login'}
+  ),
+  user.userLogin
+)
 
 module.exports = router;
---
+++ controllers/users.js
@@ -0,0 +1,48 @@
+const User = require('../models/user');
+
+const registerUser = (req,res) => {
+  res.render('users/register');
+}
+
+const loginUser = (req,res) => {
+  res.render('users/login');
+}
+
+const logoutUser = (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+        return next(err);
+    }
+    req.flash('success', 'Goodbye!');
+    res.redirect('/campgrounds');
+  });
+}
+
+const userRegister = async(req,res) => {
+  try{
+    const {username, email, password} = req.body;
+    const user = new User({email, username});
+    const registeredUser = await User.register(user, password);
+    req.login(registeredUser, err => {
+        if(err) return next(err)
+        req.flash('success', 'register success')
+        res.redirect('/campgrounds')  
+    })
+  } catch(e) {
+    req.flash('error', e.message)
+    res.redirect('register')
+  }
+}
+
+const userLogin = (req,res) => {
+  req.flash("success", "made it");
+  res.redirect('/campgrounds')
+}
+
+module.exports = {
+  registerUser,
+  loginUser,
+  logoutUser,
+  userRegister,
+  userLogin
+}
