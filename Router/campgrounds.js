e159ac29264c9df5dbfe2c0ce73633f980a970f3
chanyoungcoding <white1614@naver.com>
2023-07-24 11:27:21 +0900

    route설정완료

--- Router/campgrounds.js
+++ Router/campgrounds.js
@@ -10,18 +10,18 @@ const catchAsync = require("../utils/catchAsync");
 const { isLoggedIn, validateCampground, isAuthor } = require('../utils/middleware');
 
 //Route
-router.get("/", catchAsync(campgrounds.indexCampground));
+
+router.route('/')
+  .get(catchAsync(campgrounds.indexCampground))
+  .post(validateCampground, catchAsync(campgrounds.createNewCampground))
 
 router.get("/new", isLoggedIn , campgrounds.newCampground);
 
-router.post("/", validateCampground, catchAsync(campgrounds.createNewCampground));
-
-router.get("/:id", catchAsync(campgrounds.showCampground));
+router.route('/:id')
+  .get(catchAsync(campgrounds.showCampground))
+  .put(isLoggedIn , isAuthor , validateCampground , catchAsync(campgrounds.updateCampground))
+  .delete(isLoggedIn , isAuthor , catchAsync(campgrounds.deleteCampground))
 
 router.get("/:id/edit", isLoggedIn , isAuthor ,catchAsync(campgrounds.editCampground));
-
-router.put("/:id", isLoggedIn , isAuthor , validateCampground , catchAsync(campgrounds.updateCampground));
-
-router.delete("/:id", isLoggedIn , isAuthor , catchAsync(campgrounds.deleteCampground));
 
 module.exports = router
