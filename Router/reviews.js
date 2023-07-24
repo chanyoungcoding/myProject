93bfdef21583b6c42419c4144c33240c30e8023d
chanyoungcoding <white1614@naver.com>
2023-07-24 10:54:01 +0900

    review controllers create

--- Router/reviews.js
+++ Router/reviews.js
@@ -2,29 +2,13 @@ const express = require("express");
 const router = express.Router({mergeParams: true});
 
 //내가 불러온 것
-const Campground = require("../models/campground");
+const review = require('../controllers/reviews');
+
 const catchAsync = require("../utils/catchAsync");
-const Review = require("../models/review");
 const { validateReview, isLoggedIn } = require('../utils/middleware')
 
-router.post("/", isLoggedIn ,validateReview , catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id);
-    const review = new Review(req.body.review);
-    review.author = req.user._id;
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-  })
-);
+router.post("/", isLoggedIn ,validateReview , catchAsync(review.postReview));
 
-router.delete("/:reviewId", isLoggedIn ,catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/campgrounds/${id}`);
-  })
-);
+router.delete("/:reviewId", isLoggedIn ,catchAsync(review.deleteReview));
 
 module.exports = router;
---
+++ controllers/reviews.js
@@ -0,0 +1,25 @@
+const Campground = require("../models/campground");
+const Review = require("../models/review");
+
+const postReview = async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  const review = new Review(req.body.review);
+  review.author = req.user._id;
+  campground.reviews.push(review);
+  await review.save();
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+}
+
+const deleteReview = async (req, res) => {
+  const { id, reviewId } = req.params;
+  await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+  await Review.findByIdAndDelete(reviewId);
+  res.redirect(`/campgrounds/${id}`);
+}
+
+module.exports = {
+  postReview,
+  deleteReview
+}
