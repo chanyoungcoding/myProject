edecb369c02cb0dda4284be5afea1b081c900609
chanyoungcoding <white1614@naver.com>
2023-08-18 17:42:31 +0900

    캠핑장 detail 한 부분 수정

--- public/js/clusterMap.js
+++ public/js/clusterMap.js
@@ -3,7 +3,7 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/dark-v11',
-    center: [-103.5917, 40.6699],
+    center: [126.970874, 37.553792],
     zoom: 3
 });
 
