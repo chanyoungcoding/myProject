08a1e20e9fdf6b01d94b097b6abb05dd1354b827
chanyoungcoding <white1614@naver.com>
2023-08-29 14:31:11 +0900

    home 수정

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,24 @@
 const imgH1 = document.querySelector('.img__h1');
+const badgeEl = document.querySelector('.home__badgeEl');
 
+// center font
 gsap.to(imgH1, 2, {
   opacity: 1,
 });
+
+// badge 
+window.addEventListener('scroll', _.throttle(() => {
+  console.log('event')
+  if(window.scrollY > 700) {
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    })
+  } else {
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display:'flex'
+    })
+  }
+}, 300))
+
