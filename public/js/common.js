544b8e6801919cd94ba367a035531b9efe881e16
chanyoungcoding <white1614@naver.com>
2023-09-05 11:35:56 +0900

    main 페이지 수정 완료

--- public/js/common.js
+++ public/js/common.js
@@ -1,13 +1,13 @@
+const navbar = document.querySelector('.navbar')
 
-// navbar
-
-window.addEventListener('scroll', () => {
-  const navbar = document.querySelector('.navbar')
+// 윈도우 스크롤 시 navbar 고정
+function handleWindowScroll() {
   const scrollTop = window.scrollY;
-
   if (scrollTop > 100) {
       navbar.classList.add('fixed');
   } else {
       navbar.classList.remove('fixed');
   }
-});
+}
+
+window.addEventListener('scroll', handleWindowScroll); 
